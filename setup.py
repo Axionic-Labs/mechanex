ae6a0e1d367b480ad5a65e7e3568bf008b26ba7b
Anugyan <anugyandas@Anugyans-MacBook-Pro.local>
2026-03-20 11:41:18 +0530

    changing the version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name="mechanex",
-    version="1.0.0",
+    version="1.0.1",
     author="Axionic Labs",
     author_email="contact@axioniclabs.ai",
     description="A Python client for the Axionic API.",
